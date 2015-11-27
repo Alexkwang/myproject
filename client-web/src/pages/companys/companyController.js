dcb9830fab9bf340087e47ef7cd33f9464ef3152
Alex.K.Wang <wind.kaiable@gmail.com>
2015-11-27 23:00:27 +0800

    s

--- client-web/src/pages/companys/companyController.js
+++ client-web/src/pages/companys/companyController.js
@@ -7,33 +7,33 @@ angular.module('scotchApp').controller('companyController', ['$scope',function($
 
    
     
-  	var radarChartData = {
-		labels: ["收益率", "快乐度", "工作强度", "技术进步性", "事业成长性"],
-		datasets: [
-			{
-				label: "公司理念",
-				fillColor: "rgba(32,115,223,0.5)",
-				strokeColor: "rgba(220,220,220,1)",
-				pointColor: "rgba(220,220,220,1)",
-				pointStrokeColor: "#fff",
-				pointHighlightFill: "#fff",
-				pointHighlightStroke: "rgba(220,220,220,1)",
-				data: [75,89,67,81,76]
+ //  	var radarChartData = {
+	// 	labels: ["收益率", "快乐度", "工作强度", "技术进步性", "事业成长性"],
+	// 	datasets: [
+	// 		{
+	// 			label: "公司理念",
+	// 			fillColor: "rgba(32,115,223,0.5)",
+	// 			strokeColor: "rgba(220,220,220,1)",
+	// 			pointColor: "rgba(220,220,220,1)",
+	// 			pointStrokeColor: "#fff",
+	// 			pointHighlightFill: "#fff",
+	// 			pointHighlightStroke: "rgba(220,220,220,1)",
+	// 			data: [75,89,67,81,76]
 				
-			}
+	// 		}
 		
-		]
-	};
+	// 	]
+	// };
 
 
-	$scope.refresh = function () {
-		window.myRadar = new Chart($("#canvas")[0].getContext("2d")).Radar(radarChartData, {
-				responsive: true
-			});
+	// $scope.refresh = function () {
+	// 	window.myRadar = new Chart($("#canvas")[0].getContext("2d")).Radar(radarChartData, {
+	// 			responsive: true
+	// 		});
 
-	};
-   $scope.refresh();
-   $scope.$on("refresh", $scope.refresh);
+	// };
+ //   $scope.refresh();
+ //   $scope.$on("refresh", $scope.refresh);
 
     /*==========end your business===================================*/
 }]);
